3943f004b46b82b9c61199385b383c0afb85ad15
马军伟 <417511458@qq.com>
2019-04-05 21:53:00 +0800

    优化cms

--- jbone-cms/jbone-cms-portal/src/main/java/cn/jbone/cms/portal/controller/TagController.java
+++ jbone-cms/jbone-cms-portal/src/main/java/cn/jbone/cms/portal/controller/TagController.java
@@ -1,5 +1,6 @@
 package cn.jbone.cms.portal.controller;
 
+import cn.jbone.cms.common.dataobject.config.ArticleFiledConfigDO;
 import cn.jbone.cms.common.dataobject.search.ArticleSearchDO;
 import cn.jbone.cms.portal.service.ArticleService;
 import cn.jbone.cms.portal.service.CommonService;
@@ -31,6 +32,7 @@ public class TagController {
         int pageNumber = (p == null) ? 1 : p;
 
         ArticleSearchDO articleSearchDO = ArticleSearchDO.build(pageNumber);
+        articleSearchDO.setConfig(ArticleFiledConfigDO.buildAll().includeContent(false));
 
         if(tagId > 0){
             articleSearchDO.setTagIds(Arrays.asList(tagId));
